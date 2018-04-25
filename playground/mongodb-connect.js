2d15b9a57aafcad62b7e7af9841bb8d1f36befce
Dan Savitzky <dans@DANS-MBP-PA.local>
2018-04-25 13:56:09 -0700

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    return console.log('Unable to connect to mongodb server.');
+  }
+  console.log('Connected to mongodb server.');
+  const db = client.db('ToDoApp');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: 'false'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  // db.collection('Users').insertOne({
+  //
+  //   name: 'Dan',
+  //   age: 27,
+  //   location: 'San Francisco'
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to connec to server', err);
+  //   }
+  //   console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+  // });
+
+  client.close();
+});
