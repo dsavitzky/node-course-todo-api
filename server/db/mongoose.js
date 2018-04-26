1456408aa04c4362691f5299f316575ba13e2589
Dan Savitzky <dans@DANS-MBP-PA.local>
2018-04-26 10:20:11 -0700

    Handle local/prod db url declaration

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin:password123@ds259109.mlab.com:59109/node-todo-api'); //'mongodb://localhost:27017/TodoApp'
+
+const dbUrl = process.env.PORT ? 'mongodb://admin:password123@ds259109.mlab.com:59109/node-todo-api' : 'mongodb://localhost:27017/TodoApp';
+
+mongoose.connect(dbUrl);
 
 module.exports = { mongoose };
