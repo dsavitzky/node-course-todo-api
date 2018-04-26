5d4a50431abe997afbf3b134d5c719a1d88f1c8b
Dan Savitzky <dans@DANS-MBP-PA.local>
2018-04-26 09:19:42 -0700

    Add GET /todos/:id test cases

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todos');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -72,3 +75,32 @@ describe('GET /todos', () => {
       .end(done);
   })
 })
+
+describe('GET /todos/:id', () => {
+  it('should get the todo of the provided ID', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+
+    let id = new ObjectID();
+    request(app)
+      .get(`/todos/${id}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should throw 400 if todo is invalid id', (done) => {
+    request(app)
+      .get('/todos/12234')
+      .expect(404)
+      .end(done)
+  })
+
+});
